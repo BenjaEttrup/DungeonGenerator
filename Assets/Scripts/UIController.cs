5c57655cd81cb17bc3daefa3beea94bedd3b06b0
Benjamin <benjamin.ettrup.larsen@gmail.com>
2020-12-06 22:11:54 +0100

    Finished the game or something

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject uiImage;
+    public GameObject backButton;
+
+    public void ShowBuyMeny()
+    {
+        uiImage.SetActive(true);
+        backButton.SetActive(true);
+    }
+
+    public void HideBuyMeny()
+    {
+        uiImage.SetActive(false);
+        backButton.SetActive(false);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
